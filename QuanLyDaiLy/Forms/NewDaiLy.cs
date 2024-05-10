824bed731c81d3c054e01eea68bc6802b426e76a
leducdat-tpose <leducdat0907@gmail.com>
2024-05-10 10:54:21 +0700

    OOP for DaiLy.cs

--- QuanLyDaiLy/Forms/NewDaiLy.cs
+++ QuanLyDaiLy/Forms/NewDaiLy.cs
@@ -31,5 +31,13 @@ namespace QuanLyDaiLy
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
--- QuanLyDaiLy/Scripts/DaiLy.cs
+++ QuanLyDaiLy/Scripts/DaiLy.cs
@@ -16,5 +16,15 @@ namespace QuanLyDaiLy
         public string MaQuan { get; set; }
         public DateTime NgayTiepNhan { get; set; }
         public SqlMoney TongNo { get; set; }
+        public DaiLy(string TenDaiLy, string MaLoaiDaiLy, string DienThoai, string DiaChi, string MaQuan, DateTime NgayTiepNhan)
+        {
+            this.TenDaiLy = TenDaiLy;
+            this.MaLoaiDaiLy = MaLoaiDaiLy;
+            this.MaQuan = MaQuan;
+            this.DienThoai = DienThoai;
+            this.DiaChi = DiaChi;
+            this.NgayTiepNhan = NgayTiepNhan;
+            this.TongNo = 0;
+        }
     }
 }
