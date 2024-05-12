7ad0d02cd80a413e1226665c7edf6d17c644c6a9
phamdatzx <megumikatoubuhbuh@gmail.com>
2024-05-12 10:38:06 +0700

    Add procedure USP_GetCongNoInfo in file UPDATEQUERY

--- QueryForDatabase/UPDATEQUERY.sql
+++ QueryForDatabase/UPDATEQUERY.sql
@@ -8,3 +8,13 @@ BEGIN
     FROM DAILY inner join QUAN ON DAILY.MaQuan = QUAN.MaQuan
 	INNER JOIN LOAIDAILY ON DAILY.MaLoaiDaiLy = LOAIDAILY.MaLoaiDaiLy
 END;
+
+--Lay thong tin danh sach cong no
+CREATE PROCEDURE USP_GetCongNoInfo
+@Thang int,
+@Nam int
+AS
+BEGIN
+    SELECT TenDaiLy, NoDau, PhatSinh, NoCuoi
+    FROM DAILY inner join BAOCAOCONGNO ON DAILY.MaDaiLy = BAOCAOCONGNO.MaDaiLy
+END;
