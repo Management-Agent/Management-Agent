20914931e41b042bb938c7ffe318fc2bd73e7cce
phamdatzx <megumikatoubuhbuh@gmail.com>
2024-05-22 15:30:35 +0700

    BaoCaoCongNo add print

--- QuanLyDaiLy/Forms/BaoCaoCongNo.cs
+++ QuanLyDaiLy/Forms/BaoCaoCongNo.cs
@@ -1,9 +1,11 @@
-﻿using QuanLyDaiLy.Scripts;
+﻿using OfficeOpenXml;
+using QuanLyDaiLy.Scripts;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -92,6 +94,52 @@ namespace QuanLyDaiLy.Forms
         private void panel2_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel Files (*.xlsx)|*.xlsx|All Files (*.*)|*.*";
+            saveFileDialog.FileName = "MyFile.xlsx";
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                // Thực hiện các thao tác lưu file tại đường dẫn được chọn
+                SaveExcelFile(filePath);
+            }
+        }
+
+        private void SaveExcelFile(string filePath)
+        {
+            using (var package = new ExcelPackage())
+            {
+                // Tạo một worksheet mới
+                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                // Thêm tiêu đề
+                for (int i = 0; i < dataGridView1.Columns.Count; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = dataGridView1.Columns[i].HeaderText;
+                }
+
+                // Thêm dữ liệu
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        worksheet.Cells[i + 2, j + 1].Value = dataGridView1[j, i].Value;
+                    }
+                }
+
+                // Lưu file Excel
+                var fileName = filePath;
+                File.Delete(fileName);
+                package.SaveAs(new FileInfo(fileName));
+
+                MessageBox.Show("Dữ liệu đã được xuất ra Excel thành công!");
+            }
         }
     }
 }
