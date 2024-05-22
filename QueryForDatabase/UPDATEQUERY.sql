bedbb4a138fcf8f477c1fb7c56dc66aeb8d4f967
Minh Hoang <166412778+Gnaoh1324@users.noreply.github.com>
2024-05-22 10:09:41 +0700

    Update UPDATEQUERY.sql

--- QueryForDatabase/UPDATEQUERY.sql
+++ QueryForDatabase/UPDATEQUERY.sql
@@ -455,7 +455,7 @@ BEGIN
 	WHERE SoPhieuXuat = @SoPhieuXuat and MaMatHangXuat = @MaMatHangXuat
 END
 -------------------------------------------------
-CREATE PROCEDURE Update_CT_PXH_SLX
+CREATE PROCEDURE Update_CT_PXH_DGX
 	@SoPhieuXuat VARCHAR(10),
 	@MaMatHangXuat VARCHAR(50),
 	@DonGiaXuat Money
