2dabde0ad794a7480e9d46199de060b3cc19f1fc
Minh Hoang <166412778+Gnaoh1324@users.noreply.github.com>
2024-05-25 16:05:33 +0700

    Update PHIEUXUATHANG-CT_PHIEUXUATHANG.sql

--- QueryForDatabase/PHIEUXUATHANG-CT_PHIEUXUATHANG.sql
+++ QueryForDatabase/PHIEUXUATHANG-CT_PHIEUXUATHANG.sql
@@ -24,20 +24,20 @@ GO
 ALTER TABLE	CT_PXH
 ADD CONSTRAINT ck_SlXuat CHECK (CT_PXH.SoLuongXuat >= 0);
 GO
-
---TRIGGER
-Drop TRIGGER tr_NXH
+--------------------------------------------
+drop TRIGGER tr_NXH
+  
+CREATE TRIGGER tr_NXH
 ON PHIEUXUATHANG 
-after INSERT
+FOR INSERT
 AS 
 BEGIN
-DECLARE @NgayNhapHang DATETIME
-DECLARE @NgayXuatHang DATETIME
-SELECT @NgayXuatHang = NgayXuatHang
-FROM inserted
+	DECLARE @NgayNhapHang DATETIME
+	DECLARE @NgayXuatHang DATETIME
+SELECT @NgayXuatHang = inserted.NgayXuatHang
+	FROM inserted
 SELECT @NgayNhapHang = NgayNhapHang FROM PHIEUNHAPHANG INNER JOIN CT_PNH ON PHIEUNHAPHANG.SoPhieuNhap = CT_PNH.SoPhieuNhap INNER JOIN
-MATHANG ON MATHANG.MaMatHang = CT_PNH.MaMatHang INNER JOIN CT_PXH ON CT_PXH.MaMatHangXuat = MATHANG.MaMatHang INNER JOIN PHIEUXUATHANG
-ON PHIEUXUATHANG.SoPhieuXuat = CT_PXH.SoPhieuXuat
+CT_PXH ON CT_PXH.MaMatHangXuat = CT_PNH.MaMatHang INNER JOIN inserted on inserted.SoPhieuXuat = CT_PXH.SoPhieuXuat
 if(@NgayNhapHang < @NgayXuatHang)
 BEGIN
 ROLLBACK TRANSACTION
@@ -45,62 +45,70 @@ END
 END
 GO
 ------------------------------------------------
+drop trigger tr_PHIEUXUATHANG_TongTien
 CREATE TRIGGER tr_PHIEUXUATHANG_TongTien
 ON CT_PXH
-AFTER INSERT, UPDATE, DELETE
+FOR INSERT, UPDATE, DELETE
 AS
 BEGIN 
 IF EXISTS (SELECT 1 FROM inserted)
-BEGIN 
-UPDATE PHIEUXUATHANG
-SET TongTien =(
-Select sum(ThanhTien)
-FROM CT_PXH
-WHERE CT_PXH.SoPhieuXuat = PHIEUXUATHANG.SoPhieuXuat
-)
-WHERE PHIEUXUATHANG.SoPhieuXuat IN
-(SELECT SoPhieuXuat FROM inserted)
+	BEGIN 
+	UPDATE PHIEUXUATHANG
+	SET TongTien =(
+	Select sum(ThanhTien)
+	FROM CT_PXH
+	WHERE CT_PXH.SoPhieuXuat = PHIEUXUATHANG.SoPhieuXuat
+	)
+	WHERE PHIEUXUATHANG.SoPhieuXuat IN
+	(SELECT SoPhieuXuat FROM inserted)
 END
 ELSE
 BEGIN 
-UPDATE PHIEUXUATHANG
-SET TongTien =(
-select sum(ThanhTien)
-FROM CT_PXH
-WHERE CT_PXH.SoPhieuXuat = PHIEUXUATHANG.SoPhieuXuat
-)
-WHERE PHIEUXUATHANG.SoPhieuXuat IN
-(SELECT SoPhieuXuat FROM deleted)
+	UPDATE PHIEUXUATHANG
+	SET TongTien =(
+	select sum(ThanhTien)
+	FROM CT_PXH
+	WHERE CT_PXH.SoPhieuXuat = PHIEUXUATHANG.SoPhieuXuat
+	)
+	WHERE PHIEUXUATHANG.SoPhieuXuat IN
+	(SELECT SoPhieuXuat FROM deleted)
 END
 END;
 GO
 
 --------------------------------------
+DROP TRIGGER tr_ThanhTien
+  
 CREATE TRIGGER tr_ThanhTien 
 ON CT_PXH  
-AFTER  INSERT, DELETE 
+FOR  INSERT, DELETE 
 AS
 BEGIN 
 UPDATE CT_PXH 
 SET ThanhTien = inserted.DonGiaXuat * inserted.SoLuongXuat 
-FROM CT_PXH 
-jOIN inserted ON inserted.SoPhieuXuat = CT_PXH.SoPhieuXuat AND inserted.MaMatHangXuat = CT_PXH.MaMatHangXuat 
+--FROM CT_PXH
+from inserted
+--jOIN inserted ON inserted.SoPhieuXuat = CT_PXH.SoPhieuXuat AND inserted.MaMatHangXuat = CT_PXH.MaMatHangXuat 
 UPDATE CT_PXH 
 SET ThanhTien = deleted.DonGiaXuat * deleted.SoLuongXuat 
-FROM CT_PXH 
-JOIN deleted ON deleted.SoPhieuXuat = CT_PXH.SoPhieuXuat AND deleted.MaMatHangXuat = CT_PXH.MaMatHangXuat 
+FROM deleted
+--JOIN deleted ON deleted.SoPhieuXuat = CT_PXH.SoPhieuXuat AND deleted.MaMatHangXuat = CT_PXH.MaMatHangXuat 
 END;
 GO
 -----------------------------------------------------------------------------------
+drop trigger tr_ThanhTien_Update
+  
 CREATE TRIGGER tr_ThanhTien_Update
 ON CT_PXH
 AFTER UPDATE
 AS
 BEGIN
 UPDATE CT_PXH
-SET CT_PXH.ThanhTien = CT_PXH.DonGiaXuat * CT_PXH.SoLuongXuat
+SET ThanhTien =  inserted.DonGiaXuat * inserted.SoLuongXuat
+from inserted
 END;
 GO
-DROP TRIGGER IF EXISTS tr_PXH_SotienTra;
+
+--DROP TRIGGER IF EXISTS tr_PXH_SotienTra;
 ALTER TABLE PHIEUXUATHANG DROP COLUMN ConLai;
 ALTER TABLE PHIEUXUATHANG ADD ConLai AS (TongTien - SoTienTra) PERSISTED
